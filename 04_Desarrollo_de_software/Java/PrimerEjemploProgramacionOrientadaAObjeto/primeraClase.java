49ccd47d95f493cc33a393af16c8b6f5efd6c994
cmardonesm <carlos.mardonesm14@gmail.com>
2017-10-18 21:04:54 -0300

    Juego gato

---
+++ 04_Desarrollo_de_software/Java/PrimerEjemploProgramacionOrientadaAObjeto/primeraClase.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class primeraClase here.
+ * Primera clase creada para mostrar la interfase 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class primeraClase
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class primeraClase
+     */
+    public primeraClase()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
